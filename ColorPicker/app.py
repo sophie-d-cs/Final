683b4967639fb3860ed0fddd5f157948965f3a61
sophie-d-cs <144551737+sophie-d-cs@users.noreply.github.com>
2023-12-02 01:04:13 +0000

    attempt

--- ColorPicker/app.py
+++ ColorPicker/app.py
@@ -1,6 +1,6 @@
 import os
 
-from cs50 import SQL
+#how to import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
